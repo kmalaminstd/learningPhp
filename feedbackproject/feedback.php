fca6c5f104028aa105ec95fb972dd2fef47bc169
kmalaminstd <alaminkhanstd@gmail.com>
2024-05-24 18:15:43 +0600

    include php pages in project

---
+++ feedbackproject/feedback.php
@@ -0,0 +1,21 @@
+<?php include 'inc/header.php' ?>
+
+    <div class="container">
+        <h2>Past Feedback</h2>
+
+        <!-- Feedback Placeholder -->
+        <div id="feedback-container" class="mt-3">
+            <!-- Display this when there is no feedback -->
+            <p class="lead">There is no feedback</p>
+
+            <!-- Sample Feedback Card -->
+            <div class="card my-3 w-75">
+                <div class="card-body text-center">
+                    <p class="card-text">Sample feedback text.</p>
+                    <div class="text-secondary mt-2">By <span class="feedback-name">Name</span> on <span class="feedback-date">Date</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include 'inc/footer.php' ?>
